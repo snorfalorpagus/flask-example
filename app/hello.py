be484abb119c172b947e385e24ec4f23d0a6bdde
Joshua Arnott <joshua.arnott@gigaclear.com>
2019-05-31 11:09:00 +0100

    Use jinja to escape HTML in query string

--- app/hello.py
+++ app/hello.py
@@ -1,9 +1,13 @@
 from flask import Blueprint, request
+from jinja2 import Environment
 
 blueprint = Blueprint("hello", __name__)
+
+env = Environment()
 
 
 @blueprint.route("/")
 def hello():
     name = request.args.get("name", "World")
-    return f"Hello, {name}!"
+    template = env.from_string("Hello, {{ name | e }}!")
+    return template.render(name=name)
